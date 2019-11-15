45e9e0459cbfa49af5ece7b658c37df9244dd56a
Aliesa Jackson <aliesaj2010@yahoo.com>
2019-11-15 16:33:45 -0500

    products added to table in DB, products display on console

--- bamazon.js
+++ bamazon.js
@@ -1,24 +1,70 @@
-var mysql = require("mysql");
-require("dotenv").config();
+var mysql = require('mysql');
+require('dotenv').config();
 
+//making connection to our database
+//test if the connections work
+//connection was successful, make a promt user function
 
 var connection = mysql.createConnection({
-  host: "localhost",
+	host: 'localhost',
 
-  // Your port; if not 3306
-  port: 3306,
+	// Your port; if not 3306
+	port: 3306,
 
-  // Your username
-  user: "root",
+	// Your username
+	user: 'root',
 
-  // Your password
-  password: process.env.MY_SQL_PASS,
-  //the database name
-  database: "bamazon_db"
+	// Your password
+	password: process.env.MY_SQL_PASS,
+	//the database name
+	database: 'bamazon_db'
 });
 
 connection.connect(function(err) {
-  if (err) throw err;
-  console.log("connected as id " + connection.threadId);
-  connection.end();
+	if (err) throw err;
+	console.log('connected as id ' + connection.threadId);
+	promptUser();
 });
+
+//customer vew
+//In promptUser function
+// 1- display the aval products with their ID #'s - show your inventory from DB
+function promptUser() {
+	connection.query('SELECT * FROM products', function(err, res) {
+		if (err) throw err;
+		console.log(res);
+	});
+}
+//end of prompt user function
+
+//2 - Use inquirer to ask the user for their ID# of the product they want
+//check if) their response is "Q" or "q" run connection.end
+
+//inquirer function
+function userPrompt() {
+	inquirer
+		.prompt({
+			name: 'userChoice',
+			message: 'What is the ID of the product?',
+			type: 'input'
+		})
+		.then(function(userResponse) {});
+}
+
+//3-else)query the DB to check if the ID# corresponds to an existing ID in the products
+// 3a)select * from products where ID = user response
+//4) once we get a response from database, check if the length of the response is > 0
+//4a) if not tell user the item does not exist and call the promptUser function again (kinda like recursion)
+
+//make var for ID + user response
+
+//5) use inquirer again to ask the user for how many they want
+//6) check the quantity from the DB query response, see if it's >= the # that the user wants (if statement)
+//6a) if the user request is > the aval quantity tell the user it is not in stock and cal prompt user function again
+
+//7)if there are enough in stock, calculate what the new quantity will be by subtracting the users purchase quantity from the current quantity(store this value in a variable)
+//7a) query the DB to update the quantity using UPDATE quantity to new quantity where ID is the ID the user chose
+//7b) tell the user their purchase was successful, and tell them how much they paid
+//7c) multiply the quantity by the price of the product
+
+//8) call promptUser again
--- bamazonProducts.sql
+++ bamazonProducts.sql
@@ -6,8 +6,40 @@ USE bamazon_db;
 
 CREATE TABLE products (
   id INT NOT NULL AUTO_INCREMENT,
-  flavor VARCHAR(45) NULL,
-  price DECIMAL(10,2) NULL,
-  quantity INT NULL,
+  productName VARCHAR(45) NULL,
+  deptName VARCHAR(45) NULL,
+  price DECIMAL(10, 5) DEFAULT 0 NULL,
+  stockQuantity INTEGER (500)
   PRIMARY KEY (id)
 );
+
+INSERT INTO products (productName, deptName, price, stockQuantity)
+VALUES ("iPad 32GB", "electronics", $319.98, 50);
+
+INSERT INTO products (productName, deptName, price, stockQuantity)
+VALUES ("iPad 7th Generation Case", "electronics", $45.99, 25);
+
+INSERT INTO products (productName, deptName, price, stockQuantity)
+VALUES ("iPod Touch 32GB", "electronics", $189.00, 25);
+
+INSERT INTO products (productName, deptName, price, stockQuantity)
+VALUES ("iPod Touch Case", "electronics", $19.99, 10);
+
+INSERT INTO products (productName, deptName, price, stockQuantity)
+VALUES ("Apple AirPods(Latest Model)", "electronics", $144.00, 20);
+
+INSERT INTO products (productName, deptName, price, stockQuantity)
+VALUES ("Apple Watch Series 3", "electronics", $179.00, 5);
+
+INSERT INTO products (productName, deptName, price, stockQuantity)
+VALUES ("Apple Pencil", "electronics", $94.88, 100);
+
+INSERT INTO products (productName, deptName, price, stockQuantity)
+VALUES ("Apple Magic Mouse(wireless)", "electronics", $69.00, 10);
+
+INSERT INTO products (productName, deptName, price, stockQuantity)
+VALUES ("MacBook Pro(15-inch, 256GB)", "electronics", $1,999.00, 10);
+
+INSERT INTO products (productName, deptName, price, stockQuantity)
+VALUES ("Mac Book Pro Charger", "electronics", $29.99, 50);
+
